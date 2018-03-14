b10e5c7b9780c2701bbceddcebee8b166cf1cec6
vadim.polosov <vadim.polosov.firefly@gmail.com>
2018-03-14 08:23:27 +0700

    delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,30 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+  
+  //deleteMany
+  // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
+  //   console.log(result);
+  // });
+  //
+  //deleteOne
+  // db.collection('Todos').deleteOne({ text: "Eat lunch" }).then((result) => {
+  //   console.log(result);
+  // })
+
+  //findOneAndDelete
+  // db.collection("Todos").findOneAndDelete({ completed: false }).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndDelete({ _id: new ObjectID('5aa74884fa19b509f0459844') }).then((res) => {
+    console.log(res);
+  });
+
+});
