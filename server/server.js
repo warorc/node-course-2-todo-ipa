bf9f7d0f1eed44178e1d1bdd7c42a8f52d329a45
vadim.polosov <vadim.polosov.firefly@gmail.com>
2018-03-30 21:58:13 +0700

    add post users login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -112,7 +113,7 @@ var authenticate = (req, res, next) => {
   
   User.findByToken(token).then((user) => {
     if(!user) {
-      return Promise.rejext();
+      return Promise.reject();
     }
     
     req.user = user;
@@ -126,6 +127,18 @@ var authenticate = (req, res, next) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+})
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
